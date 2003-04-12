b4b6db6f805956ddffa5d8ee599aad4a0668b577
Jeff R. Allen <jra@nella.org>
2003-04-12 22:54:13 +0000

    Added all the download directory. Synced up other files.

---
+++ content/download/index.php
@@ -0,0 +1,32 @@
+<?php
+	$gRoot = "..";
+	include("$gRoot/lib/common.inc");
+	page("Downloading Cricket");
+?>
+
+<h2> Final Releases </h2>
+
+For the truly impatient,
+<a href="http://download.sourceforge.net/cricket/cricket-1.0.3.tar.gz">click
+here</a> to get version 1.0.3, the latest final release.
+<p>
+
+All final releases of Cricket are available on
+<a href="http://download.sourceforge.net/cricket">Sourceforge's
+download server</a>. They are also available via anonymous FTP
+from <tt>download.sourceforge.net</tt>. Change directory to here:
+<tt>/pub/sourceforge/cricket</tt>.
+
+<h2>Pre-releases</h2>
+
+Pre-releases will appear here when available. Users and developers alike
+are encouraged to try these out and report any problems to the
+cricket-users mailing list.
+
+<ul>
+	<li><a href="beta/cricket-1.0.4-pre1.tar.gz">cricket-1.0.4-pre1.tar.gz</a>
+</ul>
+<!--
+-->
+
+<? endpage(__FILE__); ?>
