2513c6539776159df5245489141cf957b3f4d18e
Jeff R. Allen <jra@nella.org>
2003-04-12 20:13:38 +0000

    Added aberrant directory.

---
+++ content/aberrant/index.php
@@ -0,0 +1,36 @@
+<!doctype html public "-//w3c//dtd html 4.0 transitional//en">
+<html>
+<head>
+   <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+   <meta name="GENERATOR" content="Mozilla/4.6 [en] (X11; U; SunOS 5.5.1 sun4u) [Netscape]">
+   <meta name="Author" content="Jake Brutlag">
+   <meta name="Keywords" content="networking; holt-winters; aberrant behavhior; RRDtool">
+   <title>Resources for Aberrant Behavior Detection</title>
+</head>
+<body>
+<font size=+2>Resources for Aberrant
+Behavior Detection in Time Series for Service Network Monitoring</font><font size=+2></font>
+<p><i><font size=+2>last updated 12/4/00</font></i>
+<ul>
+<li>
+<font size=+2><a href="lisa2000talk.htm">Slides from LISA&nbsp;2000 Conference
+Talk</a></font></li>
+
+<li>
+<font size=+2><a href="lisa2000_paper.ps">Paper submitted to the LISA 2000
+Conference</a></font></li>
+
+<li>
+<font size=+2><a href="rrd_hw.htm">Notes on the implementation within RRDTOOL</a></font></li>
+
+<li>
+<font size=+2><a href="rrd-1.0.28.aberrant.patch.gz">GZIP patch file for
+RRDTOOL&nbsp; 1.0.28 code</a></font></li>
+
+<li>
+<font size=+2><a href="patch_install.txt">Directions for installing the
+RRDTOOL code patch</a></font></li>
+</ul>
+
+</body>
+</html>
